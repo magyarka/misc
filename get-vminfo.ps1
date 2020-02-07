53328c00a83e548a78f0452491591bbf5ee488b4
magyarka <ka.magyar@gmail.com>
2020-02-06 21:42:53 -0500

    Add files via upload

---
+++ get-vminfo.ps1
@@ -0,0 +1,8 @@
+﻿function get-vminfo ($param1)
+{
+    Get-VM -Name $param1 | Select-Object Name,
+        @{N="NicPortGroup";E={(Get-NetworkAdapter -VM $_.Name).NetworkName}},
+        MemoryGB,NumCpu,
+        @{N="ToolsStatus";E={$_.ExtensionData.Summary.guest.ToolsRunningStatus}}
+
+}
